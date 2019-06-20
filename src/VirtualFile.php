a4377468b0ca60104009e91b0affa5af1deca9e2
Wiktor Klonowski <wklonowski@tsukaeru.net>
2019-06-20 10:58:17 +0900

    Add saving files and directories

--- src/VirtualFile.php
+++ src/VirtualFile.php
@@ -28,6 +28,11 @@ class VirtualFile
     protected $token;
 
     /**
+     * @var string
+     */
+    protected $content;
+
+    /**
      * @var Tsukaeru\RushFiles\Client
      */
     protected $client;
@@ -85,6 +90,16 @@ class VirtualFile
         return $this->properties['InternalName'] ?? $this->properties['Id'];
     }
 
+    public function getUploadName() : ?string
+    {
+        return $this->properties['UploadName'] ?? null;
+    }
+
+    public function getName() : string
+    {
+        return $this->properties['PublicName'] ?? $this->properties['Name'];
+    }
+
     public function getShareId() : string
     {
         return $this->properties['ShareId'] ?? $this->properties['Id'];
@@ -98,5 +113,66 @@ class VirtualFile
     public function getShareTick() : int
     {
         return $this->properties['ShareTick'];
+    }
+
+    public function getSize() : int
+    {
+        return $this->properties['EndOfFile'] ?? 0;
+    }
+
+    public function getContent(bool $refresh = false)
+    {
+        if ($this->isDirectory())
+        {
+            return $this->getChildren($refresh);
+        }
+
+        if ($this->content === null || $refresh)
+        {
+            if ($this->getSize() > 0) {
+                $this->content = $this->client->GetFileContent($this->getShareId(), $this->getUploadName(), $this->domain, $this->token);
+            } else {
+                $this->content = '';
+            }
+        }
+
+        return $this->content;
+    }
+
+    public function save(string $path)
+    {
+        $path = trim($path);
+
+        if ($path[-1] === DIRECTORY_SEPARATOR) $path .= $this->getName();
+
+        if (!is_dir(dirname($path)))
+        {
+            mkdir(dirname($path), 0777, true);
+        }
+
+        if (!$this->isFile())
+        {
+            return $this->saveDir($path);
+        }
+
+        $bytes = file_put_contents($path, $this->getContent());
+
+        if ($bytes === false)
+        {
+            throw new \Exception("Error saving file {$this->properties['PublicName']} to $path"); // @codeCoverageIgnore
+        }
+
+        return $bytes;
+    }
+
+    private function saveDir(string $path)
+    {
+        $path .= DIRECTORY_SEPARATOR;
+
+        foreach ($this->getChildren() as $file) {
+            $file->save($path);
+        }
+
+        return 0;
     }
 }
--- tests/Unit/VirtualFileTest.php
+++ tests/Unit/VirtualFileTest.php
@@ -40,7 +40,117 @@ class VirtualFileTest extends TestCase
         $dirs = $share->getDirectories();
         $this->assertEquals(1, count($dirs));
         $this->assertEquals('dir', $dirs->first()->getInternalName());
+    }
 
+    public function testGetContent()
+    {
+        $client = $this->createMock(Client::class);
+        $client->method('GetFileContent')->willReturn('content');
+
+        $file = new VirtualFile([
+            'IsFile' => true,
+            'InternalName' => 'iname',
+            'UploadName' => 'uname',
+            'ShareId' => 'sId',
+            'Tick' => 1,
+            'ShareTick' => 2,
+            'EndOfFile' => 42,
+        ], 'cloudfile.jp', 'token', $client);
+
+        $content = $file->getContent();
+
+        $this->assertEquals('content', $content);
+    }
+
+    public function testGetContentEmpty()
+    {
+        $client = $this->createMock(Client::class);
+        $client->method('GetFileContent')->willReturn('content');
+
+        $file = new VirtualFile([
+            'IsFile' => true,
+            'InternalName' => 'iname',
+            'ShareId' => 'sId',
+            'Tick' => 1,
+            'ShareTick' => 2,
+            'EndOfFile' => 0,
+        ], 'cloudfile.jp', 'token', $client);
+
+        $content = $file->getContent();
+
+        $this->assertEquals('', $content);
+    }
+
+    public function testGetContentDirectory()
+    {
+        $client = $this->createMock(Client::class);
+        $share = $this->createPartialMock(VirtualFile::class, ['getChildren']);
+        $share->method('getChildren')->willReturn(collect([
+            'file' => new VirtualFile(['IsFile' => true, 'InternalName' => 'file'], 'cloudfile.jp', 'token', $client),
+            'dir' => new VirtualFile(['IsFile' => false, 'InternalName' => 'dir'], 'cloudfile.jp', 'token', $client),
+        ]));
+
+        $files = $share->getContent();
+        $this->assertEquals(2, count($files));
+    }
+
+    public function testFileSave()
+    {
+        $file = $this->createPartialMock(VirtualFile::class, ['getContent', 'getName', 'isFile']);
+        $file->method('getContent')->willReturn('content');
+        $file->method('getName')->willReturn('test.txt');
+        $file->method('isFile')->willReturn(true);
+
+        $file_system = vfsStream::setup();
+
+        $file->save($file_system->url().'/');
+        $this->assertFileExists($file_system->url() . '/test.txt');
+        $this->assertEquals('content', file_get_contents($file_system->url().'/test.txt'));
+
+        $path = $file_system->url() .'/directory/test.txt';
+        $file->save($path);
+        $this->assertFileExists($path);
+        $this->assertEquals('content', file_get_contents($path));
+    }
+
+    public function testSaveEmptyFile()
+    {
+        $file = $this->createPartialMock(VirtualFile::class, ['getContent', 'isFile', 'getName']);
+        $file->method('getContent')->willReturn('');
+        $file->method('getName')->willReturn('test.txt');
+        $file->method('isFile')->willReturn(true);
+
+        $file_system = vfsStream::setup();
+
+        $path = $file_system->url() .'/test.txt';
+        $file->save($path);
+        $this->assertFileExists($path);
+        $this->assertEquals(0, filesize($path));
+    }
+
+    public function testFileSaveDirectory()
+    {
+        $file1 = $this->createPartialMock(VirtualFile::class, ['getContent', 'getName', 'isFile']);
+        $file1->method('getContent')->willReturn('content');
+        $file1->method('getName')->willReturn('test1.txt');
+        $file1->method('isFile')->willReturn(true);
+
+        $file2 = $this->createPartialMock(VirtualFile::class, ['getContent', 'getName', 'isFile']);
+        $file2->method('getContent')->willReturn('content');
+        $file2->method('getName')->willReturn('test2.txt');
+        $file2->method('isFile')->willReturn(true);
+
+        $dir = $this->createPartialMock(VirtualFile::class, ['getChildren', 'getName', 'isFile']);
+        $dir->method('getChildren')->willReturn([$file1, $file2]);
+        $dir->method('getName')->willReturn('dir');
+        $dir->method('isFile')->willReturn(false);
+
+        $file_system = vfsStream::setup();
+        $path = $file_system->url() . DIRECTORY_SEPARATOR;
+        $dir->save($path);
+
+        $this->assertFileExists($path . 'dir/test1.txt');
+        $this->assertFileExists($path . 'dir/test2.txt');
     }
 
     /**
